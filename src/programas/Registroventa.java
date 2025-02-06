2b5b8c19298ad6cbfc93f17a0f56583a97e53487
LEONCIO <LEONCIO@LEONCIO>
2025-02-05 21:36:15 -0500

    carga inicial

---
+++ src/programas/Registroventa.java
@@ -0,0 +1,30 @@
+
+package programas;
+import java.util.Scanner;
+public class Registroventa {
+    public static void main(String[] args){
+        //declarar variables
+        String vendedor;
+        Double vt1,vt2,vt3,comision,sm,sn;
+        Scanner lectura=new Scanner(System.in);
+        //Entrada de datos
+        System.out.print("Nombre de Vendedor:");
+        vendedor = lectura.next();
+        //entrada de datos
+        System.out.print ("Sueldo Mensual:");
+        sm=lectura.nextDouble();
+        System.out.print ("Venta1:");
+        vt1=lectura.nextDouble();
+        System.out.print ("Venta2:");
+        vt2=lectura.nextDouble();
+        System.out.print ("Venta3:");
+        vt3=lectura.nextDouble();
+        //proceso de datos
+       comision=(vt1+vt2+vt3)*0.10;
+       sn=sm+comision;
+       //Salida de datos
+        System.out.println("El sueldo mensual:"+sm);
+        System.out.println("La comision de venta:"+comision);
+        System.out.println("Sueldo Neto:"+sn);
+    }
+}
